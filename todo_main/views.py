0ff2b3e54515d61f427b8b349fbcec604b1538e2
Sourav Bhattacharjee <souravbhattacharjee926@gmail.com>
2025-10-06 12:10:36 +0530

    Final commit

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,10 +1,12 @@
 
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from todos.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
-    context = {'tasks': tasks}
+    completed_tasks = Task.objects.filter(is_completed=True).order_by('-updated_at')
+    print(completed_tasks)
+    context = {'tasks': tasks, 'completed_tasks': completed_tasks}
     return render(request, "home.html", context)
